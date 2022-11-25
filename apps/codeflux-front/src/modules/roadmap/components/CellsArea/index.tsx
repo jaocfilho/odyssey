12bffef33781c8122f7a5d912fe98f11d7e5175e
jaocfilho <augustolveira95@outlook.com>
2022-11-25 23:41:20 +0000

    @codeflux/front ADD CellsArea component

---
+++ apps/codeflux-front/src/modules/roadmap/components/CellsArea/index.tsx
@@ -0,0 +1,24 @@
+import { ScrollArea } from "milky-ui";
+
+import { generateRoadmapBar } from "../../factories";
+import { CellRows } from "../CellRows";
+
+export const CellsArea = () => {
+  const purple = generateRoadmapBar({ size: 48, color: "purple" });
+  const crimson = generateRoadmapBar({ size: 72, color: "crimson" })
+  const cyan = generateRoadmapBar({ size: 34, color: "cyan" })
+  const red = generateRoadmapBar({ size: 51, color: "red" })
+
+  const rows = [
+    { cells: purple },
+    { cells: crimson },
+    { cells: cyan },
+    { cells: red },
+  ]
+
+  return (
+    <ScrollArea css={{ width: 650, height: 200 }}>
+      <CellRows rows={rows} />
+    </ScrollArea>
+  );
+};
--- apps/codeflux-front/src/modules/roadmap/components/Roadmap/index.tsx
+++ apps/codeflux-front/src/modules/roadmap/components/Roadmap/index.tsx
@@ -1,24 +1,11 @@
-import { ScrollArea } from "milky-ui";
+import { Flex } from "milky-ui";
 
-import { generateRoadmapBar } from "../../factories";
-import { CellRows } from "../CellRows";
+import { CellsArea } from "../CellsArea";
 
 export const Roadmap = () => {
-  const purple = generateRoadmapBar({ size: 48, color: "purple" });
-  const crimson = generateRoadmapBar({ size: 72, color: "crimson" })
-  const cyan = generateRoadmapBar({ size: 34, color: "cyan" })
-  const red = generateRoadmapBar({ size: 51, color: "red" })
-
-  const rows = [
-    { cells: purple },
-    { cells: crimson },
-    { cells: cyan },
-    { cells: red },
-  ]
-
   return (
-    <ScrollArea css={{ width: 650, height: 200 }}>
-      <CellRows rows={rows} />
-    </ScrollArea>
+    <Flex>
+      <CellsArea />
+    </Flex>
   );
 };
