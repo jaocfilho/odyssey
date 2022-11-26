9c9b30f496bad6c6fce50669ca4de9bd9fb8b593
jaocfilho <augustolveira95@outlook.com>
2022-11-26 17:39:01 +0000

    @milky-ui/core ADD variants to Text component

--- packages/milky-ui-core/src/components/Text/index.tsx
+++ packages/milky-ui-core/src/components/Text/index.tsx
@@ -1,14 +1,10 @@
 import { TextElement, TextElementProps } from "./styles";
 
-type TextVariants = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-
-export type TextProps = TextElementProps & {
-  variant?: TextVariants;
-};
+export type TextProps = TextElementProps;
 
 /**
  * Text is the used to render text and paragraphs within an interface.
  */
-export const Text = ({ variant = "p", ...rest }: TextProps) => {
-  return <TextElement as={variant} {...rest} />;
+export const Text = (props: TextProps) => {
+  return <TextElement {...props} />;
 };
--- packages/milky-ui-core/src/components/Text/styles.ts
+++ packages/milky-ui-core/src/components/Text/styles.ts
@@ -3,8 +3,25 @@ import type { ComponentProps } from "../../utils/types";
 import { styled } from "../../styles/stiches";
 
 export const TextElement = styled("p", {
-    // css reset
-    all: "unset",
+  // css reset
+  all: "unset",
+
+  variants: {
+    variant: {
+      h1: {
+        fontSize: "$4xl",
+      },
+      h2: {
+        fontSize: "$2xl",
+      },
+      h3: {
+        fontSize: "$xl",
+      },
+      h4: {
+        fontSize: "$lg",
+      },
+    },
+  },
 });
 
 export type TextElementProps = ComponentProps<typeof TextElement>;
