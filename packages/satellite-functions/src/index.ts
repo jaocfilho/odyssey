7d2d3738bb137e57a30dce1abec4599f5dd06947
jaocfilho <augustolveira95@outlook.com>
2023-01-12 21:20:46 +0000

    @satellite/function CHANGE moved callMultipleTimes to its own directory

--- packages/satellite-functions/src/index.ts
+++ packages/satellite-functions/src/index.ts
@@ -1,15 +1,1 @@
-type CallMultipleTImesCallback = (index: number) => void;
-
-/**
- * Run a given callback multiple times.
- *
- * @param callback The callback to be called on each iteration.
- * @param calls The number of calls.
- *
- */
-export const callMultipleTimes = (
-  callback: CallMultipleTImesCallback,
-  calls: number
-) => {
-  [...new Array(calls)].forEach((_, index) => callback(index));
-};
+export * from './callMultipleTimes';
