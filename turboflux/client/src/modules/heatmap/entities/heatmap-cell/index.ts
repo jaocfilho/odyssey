fa130a33e86756944bf4afe0c244561d59d027e1
jaocfilho <augustolveira95@outlook.com>
2023-02-09 23:42:28 +0000

    @turboflux/client ADD zod heatmap entities

---
+++ turboflux/client/src/modules/heatmap/entities/heatmap-cell/index.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+import { themeColors } from '../../../theme';
+
+const cellTypes = ['starting', 'progress', 'ending', 'solo', 'empty'] as const;
+
+const type = z.enum(cellTypes);
+const color = z.enum(themeColors);
+
+export const heatmapCellSchema = z.object({
+  color,
+  type,
+});
+
+export type HeatmapCell = z.infer<typeof heatmapCellSchema>;
