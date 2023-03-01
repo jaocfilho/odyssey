f4a8ae6935b907522351e7eac55b1951b76aa32c
jaocfilho <augustolveira95@outlook.com>
2023-03-01 23:12:20 +0000

    @turboflux/client ADD addColumnToKanbanBoard service

--- turboflux/client/src/modules/kanban/entities/kanban-board/services/index.spec.ts
+++ turboflux/client/src/modules/kanban/entities/kanban-board/services/index.spec.ts
@@ -1,9 +1,13 @@
 import { describe, expect, it, vi } from 'vitest';
 
 import { kanbanBoardSchema } from '../entity';
-import { createKanbanBoard } from '.';
+import { addColumnToKanbanBoard, createKanbanBoard } from '.';
 
-import { generateRandomKanbanBoardInput } from '../factories';
+import {
+  generateRandomKanbanBoard,
+  generateRandomKanbanBoardInput,
+} from '../factories';
+import { generateRandomKanbanColumn } from '../../kanban-column/factories';
 
 describe('createKanbanBoard', () => {
   it('should return a valid kanban board when given valid input', () => {
@@ -15,5 +19,28 @@ describe('createKanbanBoard', () => {
     expect(parseSpy).toHaveBeenCalledWith(boardProps);
 
     parseSpy.mockRestore();
+  });
+});
+
+describe('addColumnToKanbanBoard', () => {
+  it('should add a card to a column', () => {
+    const board = generateRandomKanbanBoard();
+    const column = generateRandomKanbanColumn();
+
+    const totalColumns = board.columns.length;
+    const updatedBoard = addColumnToKanbanBoard({ board, column });
+
+    const expected = totalColumns + 1;
+    expect(updatedBoard.columns).toHaveLength(expected);
+  });
+
+  it('should add the card to the end of the column', () => {
+    const board = generateRandomKanbanBoard();
+    const column = generateRandomKanbanColumn();
+
+    const updatedBoard = addColumnToKanbanBoard({ board, column });
+    const expected = updatedBoard.columns[updatedBoard.columns.length - 1];
+
+    expect(column).toEqual(expected);
   });
 });
--- turboflux/client/src/modules/kanban/entities/kanban-board/services/index.ts
+++ turboflux/client/src/modules/kanban/entities/kanban-board/services/index.ts
@@ -1,4 +1,7 @@
-import { KanbanBoardInput, kanbanBoardSchema } from '../entity';
+import produce from 'immer';
+
+import { KanbanColumn } from '../../kanban-column';
+import { KanbanBoard, KanbanBoardInput, kanbanBoardSchema } from '../entity';
 
 type CreateKanbanBoardParams = {
   boardProps: KanbanBoardInput;
@@ -8,4 +11,18 @@ export const createKanbanBoard = ({ boardProps }: CreateKanbanBoardParams) => {
   const kanbanBoard = kanbanBoardSchema.parse(boardProps);
 
   return kanbanBoard;
+};
+
+type AddColumnToKanbanBoardParams = {
+  board: KanbanBoard;
+  column: KanbanColumn;
+};
+
+export const addColumnToKanbanBoard = ({
+  board,
+  column,
+}: AddColumnToKanbanBoardParams) => {
+  return produce(board, (draft) => {
+    draft.columns.push(column);
+  });
 };
