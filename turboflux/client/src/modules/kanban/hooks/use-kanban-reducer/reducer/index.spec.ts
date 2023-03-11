83271b16b487392848aaafe0e16378e83527f2bf
jaocfilho <augustolveira95@outlook.com>
2023-03-11 00:21:04 +0000

    @turboflux/client ADD tests to other kanban reducer actions

--- turboflux/client/src/modules/kanban/hooks/use-kanban-reducer/reducer/index.spec.ts
+++ turboflux/client/src/modules/kanban/hooks/use-kanban-reducer/reducer/index.spec.ts
@@ -3,11 +3,12 @@ import { describe, it, expect } from 'vitest';
 import { generateRandomKanbanColumnInput } from '../../../entities/kanban-column';
 import { generateRandomKanbanState } from '../../../state';
 import { reducer } from '.';
+import { generateRandomKanbanCardInput } from '../../../entities/kanban-card';
 
 describe('reducer', () => {
-  const draft = generateRandomKanbanState();
-
   it('should add a column to the board when action is ADD_COLUMN', () => {
+    const draft = generateRandomKanbanState();
+
     const columnProps = generateRandomKanbanColumnInput();
 
     const nextState = reducer(draft, {
@@ -23,6 +24,8 @@ describe('reducer', () => {
   });
 
   it('should remove a column from the board when action is REMOVE_COLUMN', () => {
+    const draft = generateRandomKanbanState();
+
     const columnToRemove = draft.board.columns[0];
 
     const nextState = reducer(draft, {
@@ -35,5 +38,62 @@ describe('reducer', () => {
     );
 
     expect(expected).toBeUndefined();
+  });
+
+  it('should add a card to a column on the board when the action is ADD_CARD', () => {
+    const draft = generateRandomKanbanState();
+
+    const column = draft.board.columns[0];
+    const cardProps = generateRandomKanbanCardInput();
+    const payload = { cardProps, column };
+
+    const nextState = reducer(draft, {
+      type: 'ADD_CARD',
+      payload,
+    });
+
+    const updatedColumn = nextState.board.columns[0];
+
+    const lastIndex = updatedColumn.cards.length - 1;
+    const expectedCard = updatedColumn.cards[lastIndex];
+
+    expect(expectedCard.id).toEqual(cardProps.id);
+  });
+
+  it('should remove a card from a column on the board when the action is REMOVE_CARD', () => {
+    const draft = generateRandomKanbanState();
+
+    const column = draft.board.columns[0];
+    const cardId = column.cards[0].id;
+
+    const payload = { cardId, column };
+
+    const nextState = reducer(draft, {
+      type: 'REMOVE_CARD',
+      payload,
+    });
+
+    const updatedColumn = nextState.board.columns[0];
+    const isIncluded = updatedColumn.cards.find((card) => card.id === cardId);
+
+    expect(isIncluded).toBeUndefined();
+  });
+
+  it('should move a column from the board when the action is MOVE_COLUMN', () => {
+    const draft = generateRandomKanbanState();
+
+    const from = 0;
+    const to = draft.board.columns.length - 1;
+    const payload = { from, to };
+
+    const targetedColumn = draft.board.columns[from];
+    const nextState = reducer(draft, {
+      type: 'MOVE_COLUMN',
+      payload,
+    });
+
+    const expected = nextState.board.columns[to];
+
+    expect(targetedColumn).toEqual(expected);
   });
 });
