dddf6220a2c96e3727cd6d4a0831967ac635efa0
jaocfilho <augustolveira95@outlook.com>
2023-03-31 22:47:17 +0000

    @aihub/client ADD tailwind

---
+++ apps/aihub/client/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind config is required for editor support
+
+const sharedConfig = require('tailwind-config/tailwind.config.js');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...sharedConfig,
+  extend: {
+    fontFamily: {
+      sans: ['var(--font-inter)'],
+    },
+  },
+};
