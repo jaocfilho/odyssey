0d0e8fae7619ffc44ca979432fcc1a201e9ca796
jaocfilho <augustolveira95@outlook.com>
2023-04-10 02:22:45 +0000

    UPDATE input style

--- apps/aihub/client/src/modules/models/components/NewModelForm/index.tsx
+++ apps/aihub/client/src/modules/models/components/NewModelForm/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -58,9 +60,11 @@ export const NewModelForm = () => {
         </Form.Field>
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" className="text-sm font-semibold leading-6">
-            Cancel
-          </button>
+          <Link href="/dashboard">
+            <button type="button" className="text-sm font-semibold leading-6">
+              Cancel
+            </button>
+          </Link>
           <button
             type="submit"
             className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
