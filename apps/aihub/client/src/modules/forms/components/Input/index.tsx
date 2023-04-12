4453bc9100a4e908cf7b2c80e487dc8e480dcc64
jaocfilho <augustolveira95@outlook.com>
2023-04-12 00:37:51 +0000

    @aihub/client UPDATE TextArea component styles

--- apps/aihub/client/src/modules/forms/components/Input/index.tsx
+++ apps/aihub/client/src/modules/forms/components/Input/index.tsx
@@ -17,7 +17,7 @@ export function Input(props: InputProps) {
     <div className="relative">
       <input
         id={props.name}
-        className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 bg-white text-zinc-500 ring-zinc-900/10 transition focus:ring-2 focus:ring-emerald-600 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:bg-zinc-800 dark:[&:not(:focus)]:ring-white/20 dark:placeholder:text-zinc-500 dark:focus:ring-emerald-500"
+        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 bg-white text-zinc-500 ring-zinc-900/10 transition focus:ring-2 focus:ring-emerald-600 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:bg-zinc-800 dark:[&:not(:focus)]:ring-white/20 dark:placeholder:text-zinc-500 dark:focus:ring-emerald-500"
         {...register(props.name)}
         {...props}
       />
