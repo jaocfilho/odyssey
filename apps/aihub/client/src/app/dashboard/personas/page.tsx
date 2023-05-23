c23b1855dfdd71f3509cd43f344d95ae2423d79d
jaocfilho <augustolveira95@outlook.com>
2023-05-23 23:28:57 +0000

    @aihub/client ADD personas page

--- apps/aihub/client/src/app/dashboard/personas/page.tsx
+++ apps/aihub/client/src/app/dashboard/personas/page.tsx
@@ -1,3 +1,9 @@
+import { CreatePersonaButton } from '@/modules/personas/components/CreatePersonaButton';
+
 export default function Personas() {
-  return <div className="flex flex-col m-4 gap-4">apps page</div>;
+  return (
+    <div className="flex flex-col m-4 gap-4">
+      <CreatePersonaButton />
+    </div>
+  );
 }
