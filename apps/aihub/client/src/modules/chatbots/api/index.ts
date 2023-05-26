491bc6cd4edeb181f4bd862fdf7deabc3a8a52a5
jaocfilho <augustolveira95@outlook.com>
2023-05-26 23:14:31 +0000

    @aihub/client ADD versioning to api folder

--- apps/aihub/client/src/modules/chatbots/api/index.ts
+++ apps/aihub/client/src/modules/chatbots/api/index.ts
@@ -1,5 +1,11 @@
-import { ChatbotsRow, Supabase } from '@/lib/supabase/types';
+import { baseSelectById, type BaseSelectByIdParams } from '@/lib/supabase/api';
+import { Supabase } from '@/lib/supabase/types';
 
-export type BaseSelectChatbotByIdParams = Pick<ChatbotsRow, 'id'>;
+export type BaseSelectChatbotByIdParams = Pick<BaseSelectByIdParams, 'id'>;
 
-export async function baseSelectChatbotById(supabase: Supabase) {}
+export async function baseSelectChatbotById(
+  { id }: BaseSelectChatbotByIdParams,
+  supabase: Supabase
+) {
+  return await baseSelectById({ id, table: 'chatbots' }, supabase);
+}
