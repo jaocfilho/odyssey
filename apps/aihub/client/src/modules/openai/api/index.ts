e640c7f6dff70b9b436fb35feb5bc07cdb56d5d6
jaocfilho <augustolveira95@outlook.com>
2023-05-26 22:42:39 +0000

    @aihub/client working example of chatCompletion with persona

--- apps/aihub/client/src/modules/openai/api/index.ts
+++ apps/aihub/client/src/modules/openai/api/index.ts
@@ -1,35 +1,20 @@
+import { createPersonaChatMessages } from '@/modules/personas/helpers';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  PromptTemplate,
-  SystemMessagePromptTemplate,
-} from 'langchain/prompts';
-import { HumanChatMessage, SystemChatMessage } from 'langchain/schema';
+import { HumanChatMessage } from 'langchain/schema';
 
 type ChatCompletionParams = {
   text: string;
 };
 
 export async function chatCompletion({ text }: ChatCompletionParams) {
-  const initialMessage = new SystemChatMessage(
-    'You are a chatbot and you will receive structions to fine tune your answer.'
-  );
-
-  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-    HumanMessagePromptTemplate.fromTemplate('{text}'),
-  ]);
-
-  const formatedPrompt = await chatPrompt.formatPromptValue({
-    text,
+  const messages = await createPersonaChatMessages({
+    domain: 'technology',
+    topic: 'software programming',
+    answer_size: 'short',
   });
-  const messages = formatedPrompt.toChatMessages();
 
   const chat = new ChatOpenAI();
-  const response = await chat.call([
-    initialMessage,
-    new HumanChatMessage(text),
-  ]);
+  const response = await chat.call([...messages, new HumanChatMessage(text)]);
 
   return response;
 }
