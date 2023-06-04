e53b9118fb9fde71f76a45fbafb161c810ad1230
jaocfilho <augustolveira95@outlook.com>
2023-06-04 17:39:49 +0000

    @intellisync/client ADD ChatbotsListLoading

--- apps/intellisync/client/src/modules/chatbots/components/ChatbotsSidebar/ChatbotsListLoading/index.tsx
+++ apps/intellisync/client/src/modules/chatbots/components/ChatbotsSidebar/ChatbotsListLoading/index.tsx
@@ -1,3 +1,18 @@
+import { mapRange } from '@odyssey/arrays';
+
 export function ChatbotsListLoading() {
-  return <div className="h-2 dark:bg-zinc-800 rounded"></div>;
+  return (
+    <div className="flex flex-col animate-pulse space-y-1">
+      {mapRange(
+        (i) => (
+          <div
+            key={i}
+            role="skeleton"
+            className="h-2 dark:bg-zinc-800 rounded"
+          />
+        ),
+        5
+      )}
+    </div>
+  );
 }
