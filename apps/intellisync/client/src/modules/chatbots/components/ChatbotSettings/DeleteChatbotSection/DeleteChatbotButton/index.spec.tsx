d9a7d79dd9139dcae2c9ac2b8366d2a310fdba5c
jaocfilho <augustolveira95@outlook.com>
2023-06-07 14:07:20 +0000

    @intellisync/client ADD delete mutation to DeleteChatbotButton

--- apps/intellisync/client/src/modules/chatbots/components/ChatbotSettings/DeleteChatbotSection/DeleteChatbotButton/index.spec.tsx
+++ apps/intellisync/client/src/modules/chatbots/components/ChatbotSettings/DeleteChatbotSection/DeleteChatbotButton/index.spec.tsx
@@ -1,30 +1,41 @@
 import { cleanup, render, screen } from '@testing-library/react';
-import { describe, it, expect, afterEach } from 'vitest';
+import { describe, it, expect, afterEach, vi, beforeEach } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
 import { DeleteChatbotButton } from '.';
 import { assertButtonIsInTheDocument } from '@odyssey/tests';
+import { useDeleteChatbotById } from '@/modules/chatbots/hooks/use-delete-chatbot-by-id';
+
+vi.mock('@/modules/chatbots/hooks/use-delete-chatbot-by-id', () => ({
+  useDeleteChatbotById: vi.fn(() => ({
+    mutate: vi.fn(),
+  })),
+}));
 
 describe('DeleteChatbotButton', () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
   afterEach(() => {
     cleanup();
   });
 
   it('should render a button', () => {
-    render(<DeleteChatbotButton />);
+    render(<DeleteChatbotButton id="any" />);
 
     assertButtonIsInTheDocument('Delete my chatbot');
   });
 
   it('should not display the modal on render', () => {
-    render(<DeleteChatbotButton />);
+    render(<DeleteChatbotButton id="any" />);
 
     const element = screen.queryByText('Delete chatbot');
     expect(element).not.toBeInTheDocument();
   });
 
   it('should render the modal on button click', async () => {
-    render(<DeleteChatbotButton />);
+    render(<DeleteChatbotButton id="any" />);
 
     await userEvent.click(screen.getByText('Delete my chatbot'));
 
@@ -33,7 +44,7 @@ describe('DeleteChatbotButton', () => {
   });
 
   it('should close the modal on cancel', async () => {
-    render(<DeleteChatbotButton />);
+    render(<DeleteChatbotButton id="any" />);
 
     await userEvent.click(screen.getByText('Delete my chatbot'));
     await userEvent.click(screen.getByText('Cancel'));
@@ -43,12 +54,24 @@ describe('DeleteChatbotButton', () => {
   });
 
   it('should close the modal on confirm', async () => {
-    render(<DeleteChatbotButton />);
+    render(<DeleteChatbotButton id="any" />);
 
     await userEvent.click(screen.getByText('Delete my chatbot'));
     await userEvent.click(screen.getByText('Delete'));
 
     const element = screen.queryByText('Delete chatbot');
     expect(element).not.toBeInTheDocument();
+  });
+
+  it('should call the delete mutation on confirm', async () => {
+    const mutate = vi.fn();
+    vi.mocked(useDeleteChatbotById).mockReturnValue({ mutate } as any);
+
+    render(<DeleteChatbotButton id="any" />);
+
+    await userEvent.click(screen.getByText('Delete my chatbot'));
+    await userEvent.click(screen.getByText('Delete'));
+
+    expect(mutate).toHaveBeenCalledWith({ id: 'any' });
   });
 });
--- apps/intellisync/client/src/modules/chatbots/components/ChatbotSettings/DeleteChatbotSection/DeleteChatbotButton/index.tsx
+++ apps/intellisync/client/src/modules/chatbots/components/ChatbotSettings/DeleteChatbotSection/DeleteChatbotButton/index.tsx
@@ -2,9 +2,19 @@
 
 import { useBooleanState } from '@odyssey/react-hooks';
 import { Button, Modal } from 'tailwind-ui';
+import { useDeleteChatbotById } from '@/modules/chatbots/hooks/use-delete-chatbot-by-id';
 
-export function DeleteChatbotButton() {
+type DeleteChatbotButtonProps = {
+  id: string;
+};
+
+export function DeleteChatbotButton({ id }: DeleteChatbotButtonProps) {
   const [open, handleClose, handleOpen] = useBooleanState();
+
+  const deleteMutation = useDeleteChatbotById();
+  const deleteChatbot = () => {
+    deleteMutation.mutate({ id });
+  };
 
   return (
     <>
@@ -15,6 +25,7 @@ export function DeleteChatbotButton() {
       <Modal
         open={open}
         onClose={handleClose}
+        onConfirm={deleteChatbot}
         title="Delete chatbot"
         message="Are you sure you want to delete this chatbot?
       All of your data will be permanently removed from our servers forever. This action cannot be undone."
