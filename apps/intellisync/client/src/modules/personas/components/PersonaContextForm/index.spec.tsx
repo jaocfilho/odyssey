83c5542355226a83f5d086b775c1b4a028941f2c
jaocfilho <augustoliveira95@outlook.com>
2023-06-09 19:29:27 +0000

    @intellisync/client ADD PersonaContextForm tests

---
+++ apps/intellisync/client/src/modules/personas/components/PersonaContextForm/index.spec.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
+
+describe('PersonaContextForm', () => {
+  it('should display a textarea on initial render', () => {
+    render(<PersonaContextForm />);
+
+    const element = screen.getByRole('textbox');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should render an element with the role addRow', () => {
+    render(<PersonaContextForm />);
+
+    const element = screen.getByRole('addRow');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should display another textarea when addRow button is clicked', async () => {
+    render(<PersonaContextForm />);
+
+    const element = screen.getByRole('addRow');
+    await userEvent.click(element);
+
+    const textareas = screen.getAllByRole('textbox');
+    expect(textareas).toHaveLength(2);
+  });
+
+  it('should render an element with the role removeRow if there are more than one textareas', async () => {
+    render(<PersonaContextForm />);
+
+    const addRowButton = screen.getByRole('addRow');
+    await userEvent.click(addRowButton);
+  
+    const element = screen.getByRole('removeRow');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should remove a textarea when removeRow button is clicked', async () => {
+    render(<PersonaContextForm />);
+
+    const addRowButton = screen.getByRole('addRow');
+    await userEvent.click(addRowButton);
+
+    const removeRowButton = screen.getByRole('removeRow');
+    await userEvent.click(removeRowButton);
+
+    const textareas = screen.getAllByRole('textbox');
+    expect(textareas).toHaveLength(1);
+  });
+
+  it('should render a disabled save button when no fields are dirty', () => {
+    render(<PersonaContextForm />);
+
+    const element = screen.getByRole('button', { name: 'Save' });
+    expect(element).toBeDisabled();
+  });
+
+  it('should render a disabled cancel button when no fields are dirty', () => {
+    render(<PersonaContextForm />);
+
+    const element = screen.getByRole('button', { name: 'Cancel' });
+    expect(element).toBeDisabled();
+  });
+
+  it('should render an enabled save button when fields are dirty', async () => {
+    render(<PersonaContextForm />);
+
+    const textarea = screen.getByRole('textbox');
+    await userEvent.type(textarea, 'test');
+
+    const element = screen.getByRole('button', { name: 'Save' });
+    expect(element).toBeEnabled();
+  });
+
+  it('should reset the textarea content when cancel button is clicked', async () => {
+    render(<PersonaContextForm />);
+
+    const textarea = screen.getByRole('textbox');
+    await userEvent.type(textarea, 'test');
+
+    const cancelButton = screen.getByRole('button', { name: 'Cancel' });
+    await userEvent.click(cancelButton);
+
+    expect(textarea).toHaveValue('');
+  })
+})
