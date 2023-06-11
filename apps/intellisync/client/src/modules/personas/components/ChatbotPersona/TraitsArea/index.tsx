8149a53f6373ac0b66ab0f41df50ae54a00853d7
jaocfilho <augustoliveira95@outlook.com>
2023-06-11 23:19:12 +0000

    @intellisync/client ADD TargetAudienceForm to TraitsArea

--- apps/intellisync/client/src/modules/personas/components/ChatbotPersona/TraitsArea/index.tsx
+++ apps/intellisync/client/src/modules/personas/components/ChatbotPersona/TraitsArea/index.tsx
@@ -1,18 +1,23 @@
+'use client';
+
 import { SectionList } from '@/components/SectionList';
-import { DeleteChatbotSection } from '@/modules/chatbots/components/ChatbotSettings/DeleteChatbotSection';
+import { useTargetAudienceForm } from '../../TargetAudienceForm/use-target-audience-form';
+import { TargetAudienceForm } from '../../TargetAudienceForm';
 
 export function TraitsArea() {
+  const { methods } = useTargetAudienceForm({});
+
   return (
     <div className="-mx-4 px-4 sm:mx-0 sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2">
       <SectionList.Root>
         <SectionList.Container>
           <SectionList.Info title="Target audience" />
           <SectionList.Content>
-            <div className="sm:max-w-xl">content</div>
+            <div className="sm:max-w-xl">
+              <TargetAudienceForm methods={methods} onSubmit={console.log} />
+            </div>
           </SectionList.Content>
         </SectionList.Container>
-        <DeleteChatbotSection id="any" />
-        <DeleteChatbotSection id="any" />
       </SectionList.Root>
     </div>
   );
