97c8ad7b5ebe5c0bb6a6d7b730249813c3a3b46a
jaocfilho <augustoliveira95@outlook.com>
2023-06-14 21:14:46 +0000

    @intellisync/client ADD useIsSettingsPath

--- apps/intellisync/client/src/components/Sidebar/NavigationItems/index.tsx
+++ apps/intellisync/client/src/components/Sidebar/NavigationItems/index.tsx
@@ -6,6 +6,7 @@ import { ChatbotsLink } from '@/modules/navigation/components/ChatbotsLink';
 import { NavItem } from '../NavItem';
 import { useIsChatbotsPath } from '@/modules/chatbots/hooks/use-is-chatbots-path';
 import { SettingsLink } from '@/modules/navigation/components/SettingsLink';
+import { useIsSettingsPath } from '@/modules/settings/hooks/use-is-settings-path';
 
 export function ChatbotsPageNavItem() {
   const current = useIsChatbotsPath();
@@ -18,9 +19,11 @@ export function ChatbotsPageNavItem() {
 }
 
 export function SettingsPageNavItem() {
+  const current = useIsSettingsPath();
+
   return (
     <SettingsLink>
-      <NavItem Icon={Cog6ToothIcon} current={false} />
+      <NavItem Icon={Cog6ToothIcon} current={current} />
     </SettingsLink>
   );
 }
--- apps/intellisync/client/src/modules/settings/urls/index.ts
+++ apps/intellisync/client/src/modules/settings/urls/index.ts
@@ -3,3 +3,10 @@ const settingsPathname = 'settings';
 export const settingsUrls = {
   base: `/dashboard/${settingsPathname}` as const,
 };
+
+export function isSettingsPath(pathname: string) {
+  const splitedPathname = pathname.split('/');
+
+  const path = splitedPathname[2];
+  return path === settingsPathname;
+}
