668a59800d58b2f1640364b02b3fe16dc3edf475
jaocfilho <augustoliveira95@outlook.com>
2023-06-15 22:06:21 +0000

    @intellisync/server ADD generate_random_hex function

---
+++ apps/intellisync/server/supabase/migrations/20230615220446_create_generate_random_hex_function.sql
@@ -0,0 +1,19 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.generate_random_hex(length integer)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  random_bytes BYTEA;
+BEGIN
+  -- Generate random bytes
+  random_bytes := gen_random_bytes(length);
+
+  -- Convert bytes to hex string
+  RETURN encode(random_bytes, 'hex');
+END;
+$function$
+;
+
+
