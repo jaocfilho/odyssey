ad7e43ec915c6b3f5088aa28275a0637568aa7d2
jaocfilho <augustoliveira95@outlook.com>
2023-06-16 22:25:42 +0000

    @intellisync/client ADD getLastUsedOrganization

--- apps/intellisync/client/src/components/Topbar/OrganizationArea/index.tsx
+++ apps/intellisync/client/src/components/Topbar/OrganizationArea/index.tsx
@@ -1,24 +1,13 @@
-import { createServerSupabase } from '@/lib/supabase/server';
-import { serverSelectProfileById } from '@/modules/profiles/api/server';
 import { Organization } from '../Organization';
-
-async function getLastUsedOrganization() {
-  const supabase = createServerSupabase();
-  const { data } = await supabase.auth.getSession();
-  // user cannot be null here
-  const { user } = data.session!;
-
-  const { data: profile } = await serverSelectProfileById({ id: user.id });
-  return { profile };
-}
+import { getLastUsedOrganization } from '@/modules/organizations/helpers';
 
 export async function OrganizationArea() {
-  const { profile } = await getLastUsedOrganization();
+  const { lastUsedOrganization } = await getLastUsedOrganization();
 
   return (
     <div className="flex">
       <div className="flex items-center space-x-4">
-        <Organization organizationId={profile!.last_used_organization!} />
+        <Organization organizationId={lastUsedOrganization} />
       </div>
     </div>
   );
--- apps/intellisync/client/src/modules/organizations/helpers/index.ts
+++ apps/intellisync/client/src/modules/organizations/helpers/index.ts
@@ -1,6 +1,8 @@
 import { queryClient } from '@/lib/react-query/client';
 import { currentQueryKey } from '../query-keys';
 import { BaseSelectOrganizationByIdReturnData } from '../api/base';
+import { createServerSupabase } from '@/lib/supabase/server';
+import { serverSelectProfileById } from '@/modules/profiles/api/server';
 
 export function setCurrentOrganization(
   initialData: BaseSelectOrganizationByIdReturnData
@@ -14,4 +16,17 @@ export function getCurrentOrganization() {
   return queryClient.getQueryData<BaseSelectOrganizationByIdReturnData>(
     queryKey
   );
+}
+
+export async function getLastUsedOrganization() {
+  const supabase = createServerSupabase();
+  const { data } = await supabase.auth.getSession();
+  // user cannot be null here
+  const { user } = data.session!;
+
+  const { data: profile } = await serverSelectProfileById({ id: user.id });
+
+  const lastUsedOrganization = profile!.last_used_organization!;
+
+  return { lastUsedOrganization };
 }
