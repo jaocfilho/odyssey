1bb4f9891b0f51ebbb58f0d345f8dd51d37cd695
jaocfilho <augustoliveira95@outlook.com>
2023-06-18 22:19:07 +0000

    tailwind-ui ADD MenuItem

--- packages/tailwind-ui/src/components/Menu/index.tsx
+++ packages/tailwind-ui/src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { classNames } from '../../utils';
 import { Transition } from './Transition';
 import { MenuButton, type MenuButtonProps } from './MenuButton';
+import { MenuItem } from './MenuItem';
 
 type DropdownProps = {
   placement?: 'left' | 'right';
@@ -28,65 +29,29 @@ export function Menu({
       </div>
 
       <Transition>
-        <HuiMenu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <HuiMenu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-left divide-y divide-gray-100 dark:divide-grayScheme-50/10 dark:bg-grayScheme-900 rounded-md bg-white shadow-lg ring-1 ring-black dark:ring-grayScheme-50/10 ring-opacity-5 focus:outline-none">
           {header}
           <div className="py-1">
-            <HuiMenu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Account settings
-                </a>
-              )}
-            </HuiMenu.Item>
-            <HuiMenu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Support
-                </a>
-              )}
-            </HuiMenu.Item>
-            <HuiMenu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  License
-                </a>
-              )}
-            </HuiMenu.Item>
+            <MenuItem>Account settings</MenuItem>
+            <MenuItem>Support</MenuItem>
+            <MenuItem>License</MenuItem>
           </div>
           <div className="py-1">
-            <form method="POST" action="#">
-              <HuiMenu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full px-4 py-2 text-left text-sm'
-                    )}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </HuiMenu.Item>
-            </form>
+            <HuiMenu.Item>
+              {({ active }) => (
+                <button
+                  type="submit"
+                  className={classNames(
+                    active
+                      ? 'bg-gray-100 text-gray-900'
+                      : 'text-gray-700 dark:text-grayScheme-200',
+                    'block w-full px-4 py-2 text-left text-sm'
+                  )}
+                >
+                  Sign out
+                </button>
+              )}
+            </HuiMenu.Item>
           </div>
         </HuiMenu.Items>
       </Transition>
