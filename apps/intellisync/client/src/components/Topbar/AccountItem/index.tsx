6aa41845b30c3e28c03d4781c941e6030adbb01b
jaocfilho <augustoliveira95@outlook.com>
2023-06-19 00:17:21 +0000

    @intellisync/client ADD UserIcon to AccountItem in topbar

--- apps/intellisync/client/src/components/Topbar/AccountItem/index.tsx
+++ apps/intellisync/client/src/components/Topbar/AccountItem/index.tsx
@@ -1,16 +1,22 @@
+import { UserIcon } from '@heroicons/react/24/outline';
+
 import { Menu } from 'tailwind-ui';
 import { MenuHeader } from './MenuHeader';
 import { SignOutButton } from './SignOutButton';
 import { serverGetSessionUser } from '@/modules/auth/api/server';
 
+<UserIcon className="h-6 w-6" />;
+
 export async function AccountItem() {
   const user = await serverGetSessionUser();
+
+  const { email } = user!;
 
   return (
     <Menu
       colorScheme="gray"
-      header={<MenuHeader email={user!.email!} />}
-      button={user!.email!}
+      header={<MenuHeader email={email!} />}
+      button={<UserIcon className="h-6 w-6" />}
     >
       <div className="py-1">
         <SignOutButton />
