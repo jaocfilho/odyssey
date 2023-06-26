69d48ee6a097d3a7a2dda99bd275f9c96fd8a634
jaocfilho <augustoliveira95@outlook.com>
2023-06-26 12:37:26 +0000

    @intellisync/client ADD generic document loader function

--- apps/intellisync/client/src/modules/documents/helpers/server/index.ts
+++ apps/intellisync/client/src/modules/documents/helpers/server/index.ts
@@ -1,8 +1,15 @@
+import { BaseDocumentLoader } from 'langchain/dist/document_loaders/base';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 
-export async function loadPdf(file: Blob) {
-  const loader = new PDFLoader(file);
-
+export async function loadFile<T extends BaseDocumentLoader>(
+  file: Blob,
+  Loader: new (file: Blob) => T
+) {
+  const loader = new Loader(file);
   const docs = await loader.load();
   return docs;
+}
+
+export async function loadPdf(file: Blob) {
+  return await loadFile(file, PDFLoader);
 }
