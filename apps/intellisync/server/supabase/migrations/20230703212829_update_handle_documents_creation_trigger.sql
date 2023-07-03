be2ba6f61c785caa311b57ecc4f937bbbc1904a5
jaocfilho <augustoliveira95@outlook.com>
2023-07-03 21:46:40 +0000

    @intellisync/server FIX wrong migration

--- apps/intellisync/server/supabase/migrations/20230703212829_update_handle_documents_creation_trigger.sql
+++ apps/intellisync/server/supabase/migrations/20230703212829_update_handle_documents_creation_trigger.sql
@@ -1,12 +1,3 @@
-CREATE INDEX flow_state_created_at_idx ON auth.flow_state USING btree (created_at DESC);
-
-CREATE INDEX refresh_tokens_updated_at_idx ON auth.refresh_tokens USING btree (updated_at DESC);
-
-CREATE INDEX saml_relay_states_created_at_idx ON auth.saml_relay_states USING btree (created_at DESC);
-
-CREATE INDEX sessions_not_after_idx ON auth.sessions USING btree (not_after DESC);
-
-
 set check_function_bodies = off;
 
 CREATE OR REPLACE FUNCTION public.handle_new_document()
