19d1e5ec8b3056389001140d52a2c9cffafdab87
jaocfilho <augustoliveira95@outlook.com>
2023-07-03 20:13:07 +0000

    @intellisync/server FIX corrupted migrations

--- apps/intellisync/server/supabase/migrations/20230615231449_remote_commit.sql
+++ apps/intellisync/server/supabase/migrations/20230615231449_remote_commit.sql
@@ -1,10 +1,10 @@
-CREATE INDEX flow_state_created_at_idx ON auth.flow_state USING btree (created_at DESC);
+-- CREATE INDEX flow_state_created_at_idx ON auth.flow_state USING btree (created_at DESC);
 
-CREATE INDEX refresh_tokens_updated_at_idx ON auth.refresh_tokens USING btree (updated_at DESC);
+-- CREATE INDEX refresh_tokens_updated_at_idx ON auth.refresh_tokens USING btree (updated_at DESC);
 
-CREATE INDEX saml_relay_states_created_at_idx ON auth.saml_relay_states USING btree (created_at DESC);
+-- CREATE INDEX saml_relay_states_created_at_idx ON auth.saml_relay_states USING btree (created_at DESC);
 
-CREATE INDEX sessions_not_after_idx ON auth.sessions USING btree (not_after DESC);
+-- CREATE INDEX sessions_not_after_idx ON auth.sessions USING btree (not_after DESC);
 
 
 drop function if exists "public"."generate_random_hex"(length integer);
