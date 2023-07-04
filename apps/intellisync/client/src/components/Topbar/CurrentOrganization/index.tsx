9a3d9bb00bcdc6b5d86986036ed7377ab9dc2bba
jaocfilho <augustoliveira95@outlook.com>
2023-07-04 00:40:24 +0000

    @intellisync/client minor code changes

--- apps/intellisync/client/src/components/Topbar/CurrentOrganization/index.tsx
+++ apps/intellisync/client/src/components/Topbar/CurrentOrganization/index.tsx
@@ -6,12 +6,10 @@ export function CurrentOrganization() {
   const organizationQuery = useCurrentOrganization();
 
   return (
-    <div className="flex">
-      <div className="flex items-center space-x-4">
-        <p className="text-sm font-medium cursor-pointer dark:text-grayScheme-500 dark:hover:text-grayScheme-400 leading-6">
-          {organizationQuery.data?.name}
-        </p>
-      </div>
+    <div className="flex items-center">
+      <p className="text-sm font-medium cursor-pointer dark:text-grayScheme-500 dark:hover:text-grayScheme-400 leading-6">
+        {organizationQuery.data?.name}
+      </p>
     </div>
   );
 }
