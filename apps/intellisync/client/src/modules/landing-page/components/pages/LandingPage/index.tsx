95d65f0c5a153fd9d6c08a6bb5f1b7e1b2bb0fc4
jaocfilho <augustoliveira95@outlook.com>
2023-07-05 14:18:34 +0000

    @intellisync/client ADD Pricing section to landing page

--- apps/intellisync/client/src/modules/landing-page/components/pages/LandingPage/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/pages/LandingPage/index.tsx
@@ -1,6 +1,7 @@
 import { Features } from '../../Features';
 import { Header } from '../../Header';
 import { Hero } from '../../Hero';
+import { Pricing } from '../../Pricing';
 
 export function LandingPage() {
   return (
@@ -9,6 +10,7 @@ export function LandingPage() {
       <main>
         <Hero />
         <Features />
+        <Pricing />
       </main>
     </>
   );
