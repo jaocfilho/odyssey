0f0acac4430afa1440a5031e02d50171b6ae7284
jaocfilho <augustoliveira95@outlook.com>
2023-07-06 14:08:50 +0000

    @intellisync/client ADD RegisterLink

--- apps/intellisync/client/src/modules/auth/components/LoginForm/FreeTrial/index.tsx
+++ apps/intellisync/client/src/modules/auth/components/LoginForm/FreeTrial/index.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link';
+import { RegisterLink } from '@/modules/navigation/components/RegisterLink';
 
 export function FreeTrial() {
   return (
     <p className="mt-10 text-center text-sm text-grayScheme-500 dark:text-grayScheme-400">
       Not a member?{' '}
-      <Link
-        href="/register"
-        className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
-      >
+      <RegisterLink className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">
         Create a free account
-      </Link>
+      </RegisterLink>
     </p>
   );
 }
