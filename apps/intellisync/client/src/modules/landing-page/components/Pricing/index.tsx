645c871eaee21fbea8b9a1ddd88883c21564cf0f
jaocfilho <augustoliveira95@outlook.com>
2023-07-07 19:47:45 +0000

    @intellisync/client ADD TierCard to pricing component

--- apps/intellisync/client/src/modules/landing-page/components/Pricing/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/Pricing/index.tsx
@@ -24,32 +24,94 @@ type Tier = {
   id: string;
   href: string;
   price: { monthly: string; annually: string };
-  description: string;
   features: string[];
   mostPopular: boolean;
 };
 
+type TierCardProps = {
+  tier: Tier;
+  frequency: Frequency;
+};
+
+function TierCard({ tier, frequency }: TierCardProps) {
+  return (
+    <div
+      key={tier.id}
+      className={classNames(
+        tier.mostPopular
+          ? 'bg-white/5 ring-2 ring-indigo-500'
+          : 'ring-1 ring-white/10',
+        'rounded-3xl p-8 xl:p-10'
+      )}
+    >
+      <div className="flex items-center justify-between gap-x-4">
+        <h3 id={tier.id} className="text-lg font-semibold leading-8 text-white">
+          {tier.name}
+        </h3>
+        {tier.mostPopular ? (
+          <p className="rounded-full bg-indigo-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white">
+            Most popular
+          </p>
+        ) : null}
+      </div>
+      <p className="mt-6 flex items-baseline gap-x-1">
+        <span className="text-4xl font-bold tracking-tight text-white">
+          {tier.price[frequency.value]}
+        </span>
+        <span className="text-sm font-semibold leading-6 text-gray-300">
+          {frequency.priceSuffix}
+        </span>
+      </p>
+      <a
+        href={tier.href}
+        aria-describedby={tier.id}
+        className={classNames(
+          tier.mostPopular
+            ? 'bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500'
+            : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
+          'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
+        )}
+      >
+        Buy plan
+      </a>
+      <ul
+        role="list"
+        className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
+      >
+        {tier.features.map((feature) => (
+          <li key={feature} className="flex gap-x-3">
+            <CheckIcon
+              className="h-6 w-5 flex-none text-white"
+              aria-hidden="true"
+            />
+            {feature}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 const tiers: Tier[] = [
   {
-    name: 'Freelancer',
-    id: 'tier-freelancer',
+    name: 'Basic',
+    id: 'basic',
     href: '#',
-    price: { monthly: '$15', annually: '$144' },
-    description: 'The essentials to provide your best work for clients.',
+    price: { monthly: '$14', annually: '$140' },
     features: [
-      '5 products',
-      'Up to 1,000 subscribers',
-      'Basic analytics',
-      '48-hour support response time',
+      '3 chatbots',
+      '1,000 message credits per month',
+      '3,000,000 storage tokens per month',
+      'GPT 4',
+      'API access',
     ],
     mostPopular: false,
   },
   {
-    name: 'Startup',
-    id: 'tier-startup',
+    name: 'Standart',
+    id: 'standart',
     href: '#',
-    price: { monthly: '$30', annually: '$288' },
-    description: 'A plan that scales with your rapidly growing business.',
+    price: { monthly: '$49', annually: '$490' },
     features: [
       '25 products',
       'Up to 10,000 subscribers',
@@ -64,7 +126,6 @@ const tiers: Tier[] = [
     id: 'tier-enterprise',
     href: '#',
     price: { monthly: '$48', annually: '$576' },
-    description: 'Dedicated support and infrastructure for your company.',
     features: [
       'Unlimited products',
       'Unlimited subscribers',
@@ -122,66 +183,7 @@ export function Pricing() {
         </div>
         <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {tiers.map((tier) => (
-            <div
-              key={tier.id}
-              className={classNames(
-                tier.mostPopular
-                  ? 'bg-white/5 ring-2 ring-indigo-500'
-                  : 'ring-1 ring-white/10',
-                'rounded-3xl p-8 xl:p-10'
-              )}
-            >
-              <div className="flex items-center justify-between gap-x-4">
-                <h3
-                  id={tier.id}
-                  className="text-lg font-semibold leading-8 text-white"
-                >
-                  {tier.name}
-                </h3>
-                {tier.mostPopular ? (
-                  <p className="rounded-full bg-indigo-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white">
-                    Most popular
-                  </p>
-                ) : null}
-              </div>
-              <p className="mt-4 text-sm leading-6 text-gray-300">
-                {tier.description}
-              </p>
-              <p className="mt-6 flex items-baseline gap-x-1">
-                <span className="text-4xl font-bold tracking-tight text-white">
-                  {tier.price[frequency.value]}
-                </span>
-                <span className="text-sm font-semibold leading-6 text-gray-300">
-                  {frequency.priceSuffix}
-                </span>
-              </p>
-              <a
-                href={tier.href}
-                aria-describedby={tier.id}
-                className={classNames(
-                  tier.mostPopular
-                    ? 'bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500'
-                    : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
-                  'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                )}
-              >
-                Buy plan
-              </a>
-              <ul
-                role="list"
-                className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
-              >
-                {tier.features.map((feature) => (
-                  <li key={feature} className="flex gap-x-3">
-                    <CheckIcon
-                      className="h-6 w-5 flex-none text-white"
-                      aria-hidden="true"
-                    />
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <TierCard key={tier.id} tier={tier} frequency={frequency} />
           ))}
         </div>
       </div>
