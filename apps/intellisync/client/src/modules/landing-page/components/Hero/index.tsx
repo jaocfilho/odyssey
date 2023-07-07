c4de911a0588ca74d3efbbe07e1dfe82d9c2c902
jaocfilho <augustoliveira95@outlook.com>
2023-07-07 18:08:22 +0000

    @intellisync/client ADD FAQ area to landing page

--- apps/intellisync/client/src/modules/landing-page/components/Hero/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/Hero/index.tsx
@@ -19,7 +19,7 @@ export function Hero() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Build AI chat search powered by your data
+              Build AI chatbots powered by your data
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Create production-ready chatbots with your data. Deploy anywhere.
--- apps/intellisync/client/src/modules/landing-page/components/pages/LandingPage/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/pages/LandingPage/index.tsx
@@ -1,4 +1,5 @@
 import { CTA } from '../../CTA';
+import { FAQ } from '../../FAQ';
 import { Features } from '../../Features';
 import { Footer } from '../../Footer';
 import { Header } from '../../Header';
@@ -13,6 +14,7 @@ export function LandingPage() {
         <Hero />
         <Features />
         <Pricing />
+        <FAQ />
         <CTA />
         <Footer />
       </main>
