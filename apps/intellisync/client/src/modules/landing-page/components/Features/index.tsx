47386500c2b4769808f25fb60ae66da3890abdc2
jaocfilho <augustoliveira95@outlook.com>
2023-07-10 19:28:36 +0000

    @intellisync/client UPDATE logo size

--- apps/intellisync/client/src/modules/landing-page/components/Features/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/Features/index.tsx
@@ -67,7 +67,7 @@ export function Features() {
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
             {features.map((feature) => (
-              <FeatureCard feature={feature} />
+              <FeatureCard key={feature.name} feature={feature} />
             ))}
           </dl>
         </div>
