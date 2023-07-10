675ebfb880bbc0daf294bdb6dbfa65b16d2ed5c9
jaocfilho <augustoliveira95@outlook.com>
2023-07-10 14:32:08 +0000

    @intellisinc/client UPDATE landing page

--- apps/intellisync/client/src/modules/landing-page/components/Features/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/Features/index.tsx
@@ -58,7 +58,7 @@ export function Features() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-400">
-            Deploy faster
+            Supercharge your knowledge
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Create powerful AI knowledge bases
