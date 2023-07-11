e1e3a755c15b5287744cdc73fff96852664f639b
jaocfilho <augustoliveira95@outlook.com>
2023-07-11 11:26:48 +0000

    @intellisync/client FIX footer text

--- apps/intellisync/client/src/modules/landing-page/components/Footer/index.tsx
+++ apps/intellisync/client/src/modules/landing-page/components/Footer/index.tsx
@@ -1,30 +1,3 @@
-const navigation = {
-  solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
-  ],
-  support: [
-    { name: 'Pricing', href: '#' },
-    { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
-  ],
-  company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
-  ],
-  legal: [
-    { name: 'Claim', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
-  ],
-};
-
 export function Footer() {
   return (
     <footer aria-labelledby="footer-heading" className="relative">
@@ -34,7 +7,7 @@ export function Footer() {
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-4 lg:px-8">
         <div className="border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
           <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2023 IntellisyncAI. All rights reserved.
           </p>
         </div>
       </div>
