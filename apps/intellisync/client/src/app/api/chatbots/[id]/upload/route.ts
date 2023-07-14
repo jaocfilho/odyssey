2290a4235849ca7c3d7657c530aeae1de2d56e79
jaocfilho <augustoliveira95@outlook.com>
2023-07-14 19:17:50 +0000

    @intellisync/client FIX cors config on file upload api route

--- apps/intellisync/client/src/app/api/chatbots/[id]/upload/route.ts
+++ apps/intellisync/client/src/app/api/chatbots/[id]/upload/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { storeVectorsFromFiles } from '@/modules/documents/api/server';
 
-export const corsHeaders = {
+const corsHeaders = {
   'Access-Control-Allow-Origin': 'https://www.intellisyncai.com',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
