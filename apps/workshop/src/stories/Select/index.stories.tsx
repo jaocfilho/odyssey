b35915a0b1d66776e84b5e35421e82d5e42353ce
jaocfilho <augustoliveira95@outlook.com>
2023-07-15 19:26:20 +0000

    workshop ADD Select story

---
+++ apps/workshop/src/stories/Select/index.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Select as SelectComponent } from 'tailwind-ui';
+
+const options = [
+  { label: 'Option 1', value: 'option-1' },
+  { label: 'Option 2', value: 'option-2' },
+  { label: 'Option 3', value: 'option-3' },
+];
+
+const meta: Meta<typeof SelectComponent> = {
+  title: 'Components/Forms/Select',
+  component: SelectComponent,
+  args: {
+    options,
+    placeholder: 'Placeholder',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SelectComponent>;
+
+export const Select: Story = {
+  render: (args) => <SelectComponent {...args} />,
+};
