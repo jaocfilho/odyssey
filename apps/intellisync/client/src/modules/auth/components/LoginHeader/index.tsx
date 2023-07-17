bb86bf4e2adb9af675153a57b11d615003a3984a
jaocfilho <augustoliveira95@outlook.com>
2023-07-17 15:26:20 +0000

    @intellisync/client UPDATE logo from login page

--- apps/intellisync/client/src/modules/auth/components/LoginHeader/index.tsx
+++ apps/intellisync/client/src/modules/auth/components/LoginHeader/index.tsx
@@ -2,9 +2,9 @@ export function LoginHeader() {
   return (
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
       <img
-        className="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
+        className="mx-auto h-12 w-auto"
+        src="/LogoFull.svg"
+        alt="IntellisyncAI logo"
       />
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white">
         Sign in to your account
