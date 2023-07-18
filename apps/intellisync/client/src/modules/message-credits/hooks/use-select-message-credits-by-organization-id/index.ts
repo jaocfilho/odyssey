9502995e235885e3cf4aa129156022aa2d4e0e52
jaocfilho <augustoliveira95@outlook.com>
2023-07-18 22:25:25 +0000

    @intellisync/client ADD useOrganizationMessageCredits

--- apps/intellisync/client/src/modules/message-credits/hooks/use-select-message-credits-by-organization-id/index.ts
+++ apps/intellisync/client/src/modules/message-credits/hooks/use-select-message-credits-by-organization-id/index.ts
@@ -1,10 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+
 import { useSupabase } from '@/lib/supabase/Provider';
 import {
   baseSelectMessageCreditsByOrganizationId,
   type BaseSelectMessageCreditsByOrganizationIdParams,
 } from '../../api/base';
 import { organizationsQueryKeys } from '@/modules/organizations/query-keys';
-import { useQuery } from '@tanstack/react-query';
 
 export function useBaseSelectMessageCreditsByOrganizationId() {
   const { supabase } = useSupabase();
@@ -21,9 +22,14 @@ export function useBaseSelectMessageCreditsByOrganizationId() {
   return { selectMessageCreditsByOrganizationId };
 }
 
-export function useSelectMessageCreditsByOrganizationId({
-  organizationId,
-}: BaseSelectMessageCreditsByOrganizationIdParams) {
+type UseSelectMessageCreditsByOrganizationIdOptions = {
+  enabled?: boolean;
+};
+
+export function useSelectMessageCreditsByOrganizationId(
+  { organizationId }: BaseSelectMessageCreditsByOrganizationIdParams,
+  options?: UseSelectMessageCreditsByOrganizationIdOptions
+) {
   const { selectMessageCreditsByOrganizationId } =
     useBaseSelectMessageCreditsByOrganizationId();
 
@@ -35,5 +41,5 @@ export function useSelectMessageCreditsByOrganizationId({
     return data;
   };
 
-  return useQuery({ queryKey, queryFn });
+  return useQuery({ queryKey, queryFn, enabled: options?.enabled });
 }
