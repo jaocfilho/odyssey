0ae5ce305b40612f41b0d4095b6af75881cafa1b
jaocfilho <augustoliveira95@outlook.com>
2023-07-19 20:26:20 +0000

    @intellisync/client ADD useSelectProfileById

--- apps/intellisync/client/src/modules/profiles/hooks/use-select-profile-by-id/index.ts
+++ apps/intellisync/client/src/modules/profiles/hooks/use-select-profile-by-id/index.ts
@@ -1,8 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+
 import { useSupabase } from '@/lib/supabase/Provider';
 import {
   baseSelectProfileById,
   type BaseSelectProfileByIdParams,
 } from '../../api/base';
+import { profilesQueryKeys } from '../../query-keys';
 
 export function useBaseSelectProfileById() {
   const { supabase } = useSupabase();
@@ -12,4 +15,23 @@ export function useBaseSelectProfileById() {
   };
 
   return { selectProfileById };
+}
+
+type UseSelectProfileByIdOptions = {
+  enabled?: boolean;
+};
+
+export function useSelectProfileById(
+  { id }: BaseSelectProfileByIdParams,
+  options?: UseSelectProfileByIdOptions
+) {
+  const { selectProfileById } = useBaseSelectProfileById();
+
+  const queryKey = profilesQueryKeys.profile();
+  const queryFn = async () => {
+    const { data } = await selectProfileById({ id });
+    return data;
+  };
+
+  return useQuery({ queryKey, queryFn, enabled: options?.enabled });
 }
