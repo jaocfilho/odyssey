91e64ea2e095b47cad2dae5d145a10d12462f00a
jaocfilho <augustoliveira95@outlook.com>
2023-08-03 20:42:01 +0000

    @intellisync/client ADD remove icon to NewDocumentsListItem

--- apps/intellisync/client/src/modules/documents/components/NewDocumentsList/NewDocumentsListItem/index.tsx
+++ apps/intellisync/client/src/modules/documents/components/NewDocumentsList/NewDocumentsListItem/index.tsx
@@ -1,3 +1,5 @@
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
 export type NewDocumentsListItemProps = {
   characters: number;
   fileName: string;
@@ -9,13 +11,14 @@ export function NewDocumentsListItem({
 }: NewDocumentsListItemProps) {
   return (
     <li key={fileName} className="py-4">
-      <div className="flex items-center gap-x-3">
+      <div className="flex items-center gap-x-4">
         <h3 className="flex-auto truncate text-sm font-semibold leading-6 text-white">
           {fileName}
         </h3>
-        <p className="flex-none text-xs text-gray-500">
+        <p className="flex-none text-sm dark:text-grayScheme-400">
           {characters} characters
         </p>
+        <XMarkIcon className="h-4 w-4 cursor-pointer dark:text-grayScheme-500 dark:hover:text-grayScheme-400" />
       </div>
     </li>
   );
